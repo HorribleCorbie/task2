c9388fb5aca4f91823f6f7412298b0a22a47d476
admin <admin@DESKTOP-V0Q863D>
2026-02-19 15:27:26 +0800

    Сделаны два теста, один из них подтверждает, что метод верно считает значения, другой то, что при отсутсвии данных программа этого не скажет и просто выведет нули

--- task2Tests/LogicTests.cs
+++ task2Tests/LogicTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace task2.Tests
 {
@@ -14,7 +15,16 @@ namespace task2.Tests
         [TestMethod()]
         public void CountRatingTest()
         {
-            Assert.Fail();
+            string result = "Кол-во 5: 2\nКол-во 4: 1\nКол-во 3: 2\nКол-во 2: 0";
+            string res2 = Logic.CountRating(new int[] {5,4,5,3,3 });
+            Assert.AreEqual(result, res2);
+        }
+        [TestMethod()]
+        public void CountRatingTest1()
+        {
+            string result = "Кол-во 5: 0\nКол-во 4: 0\nКол-во 3: 0\nКол-во 2: 0";
+            string res2 = Logic.CountRating(new int[5]);
+            Assert.Fail(result, res2);
         }
     }
 }
