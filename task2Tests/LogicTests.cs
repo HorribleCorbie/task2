6525d87feb59906b96912721c6c2d67d3f892951
admin <admin@DESKTOP-V0Q863D>
2026-02-19 15:14:11 +0800

    Создан проект с тестами и перемещен внутрь основного проекта, чтобы внутри репозитория был

---
+++ task2Tests/LogicTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using task2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void CountRatingTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
