9d3d0a612f0da3894a257f28bbff87ef99d2476f
admin <admin@DESKTOP-V0Q863D>
2026-02-19 14:55:42 +0800

    Декомпозиция Логики подсчета оценок в отдельный класс

--- Program.cs
+++ Program.cs
@@ -2,22 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 //Известны оценки каждого из учеников класса. Посчитать количество пятерок, количество четверок, количество троек и количество двоек.
 
 namespace task2
 {
-    internal class Program
+    public static class Logic
     {
-        static void Main(string[] args)
+        public static string CountRating(int[] rating)
         {
-            Random rand = new Random();
-            int[] rating = new int[15];
             int five = 0, four = 0, three = 0, two = 0;
             for (int i = 0; i < rating.Length; i++)
             {
-                rating[i] = rand.Next(2, 6);
                 switch (rating[i])
                 {
                     case 2: two++; break;
@@ -27,6 +25,23 @@ namespace task2
                 }
             }
             string message = "Кол-во 5: " + five + "\nКол-во 4: " + four + "\nКол-во 3: " + three + "\nКол-во 2: " + two;
+            return message;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int[] rating = new int[10];
+            for (int i = 0; i < rating.Length; i++)
+            {
+                rating[i] = rand.Next(2, 6);
+            }
+
+            string message = Logic.CountRating(rating);
+
+
             Console.WriteLine(message);
 
         }
